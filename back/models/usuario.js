7afa214b002c681f891ed9282ef06ba8b29305ae
Laura Pedraza <laurampego@gmail.com>
2024-04-15 00:08:12 +0200

    Model de usuario

---
+++ back/models/usuario.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class usuario extends Model {
+    static associate(models) {
+      this.hasMany(models.rolAsignado, {
+        foreignKey: process.env.FK_USUARIO,
+        as: process.env.ROLES_USUARIO,
+      });
+    }
+  }
+  user.init({
+    nombre: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: process.env.MODEL_USUARIO,
+    tableName: process.env.TABLA_USUARIOS
+  });
+  return usuario;
+};
