44eb3a4dde3a75705cb9aa2b1bc6a2b0eb28e2d9
Laura Pedraza <laurampego@gmail.com>
2024-04-15 13:11:55 +0200

    Actualización del readme

--- back/factories/rolFactory.js
+++ back/factories/rolFactory.js
@@ -4,7 +4,7 @@ const rolFactory = async (ctos=4) => {
     let lista=[
         {nombre:process.env.ROL_ADMIN,},
         {nombre:process.env.ROL_JEFE_ESTUDIOS},
-        {nombre:process.env.ROL_PROFESOR},
+        {nombre:process.env.ROL_PROFE},
         {nombre:process.env.ROL_USER}
     ]
     for(let i = 0; i < ctos; i++) {
--- back/migrations/20240415101007-create-rol-usuario.js
+++ back/migrations/20240415101007-create-rol-usuario.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable(process.env.TABLA_ROL_USUARIO, {
+    await queryInterface.createTable(process.env.TABLA_ROLES_USUARIOS, {
       id: {
         allowNull: false,
         autoIncrement: true,
