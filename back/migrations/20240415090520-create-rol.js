874c878ae20bbc472b3762d2b73d8e1eafb131e5
Laura Pedraza <laurampego@gmail.com>
2024-04-15 11:07:49 +0200

    Migration de rol

---
+++ back/migrations/20240415090520-create-rol.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable(process.env.TABLA_ROLES, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable(process.env.TABLA_ROLES);
+  }
+};
