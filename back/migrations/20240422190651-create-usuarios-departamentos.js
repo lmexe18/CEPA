1f5bdf4760cf8365a8853e3d935728ecb9778592
Laura Pedraza <laurampego@gmail.com>
2024-04-22 21:25:30 +0200

    Migration de usuarios departamentos

---
+++ back/migrations/20240422190651-create-usuarios-departamentos.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable(process.env.TABLA_DEPA_USER, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      usuarioId: {
+        type: Sequelize.INTEGER
+      },
+      departamentoId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable(process.env.TABLA_DEPA_USERS);
+  }
+};
