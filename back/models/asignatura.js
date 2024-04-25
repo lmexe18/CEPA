2fbdb7d5b2f0b288bb55363bc73fe4c669b96344
Laura Pedraza <laurampego@gmail.com>
2024-04-25 19:43:48 +0200

    Model de asignatura

---
+++ back/models/asignatura.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class asignatura extends Model {
+
+    static associate(models) {
+      // Una asignatura pertenece a un departamento
+      asignatura.belongsTo(models.departamento, {
+        foreignKey: process.env.ID_DEPARTAMENTO,
+        as: 'asignaturaDepartamento'
+      });
+
+      // Una asignatura tiene muchos temarios
+      asignatura.hasMany(models.temario, {
+        foreignKey: process.env.ID_ASIGNATURA,
+        as: 'asignaturaTemario'
+      });
+
+      // Una asignatura tiene muchos grupos
+      asignatura.belongsToMany(models.grupo, {
+        through: models.asignaturaGrupo,
+        foreignKey: process.env.ID_ASIGNATURA,
+        as: 'asignaturaGrupo'
+      });
+
+      // Una asignatura tiene muchos profesores
+      asignatura.belongsToMany(models.usuario, {
+        through: models.asignaturaGrupo,
+        foreignKey: process.env.ID_ASIGNATURA,
+        as: 'asignaturaProfesor'
+      });
+    }
+  }
+  Asignatura.init({
+    nombre: DataTypes.STRING,
+    departamentoId: DataTypes.INTEGER,
+    activado: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: process.env.MODEL_ASIGNATURA,
+    tableName: process.env.TABLA_ASIGNATURAS
+  });
+  return asignatura;
+};
